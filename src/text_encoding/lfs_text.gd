3d4bfb0c9843cc93adc3cce2431d0b2d00f1802d
Cyril K.B. <53737317+Cykyrios@users.noreply.github.com>
2024-03-23 14:44:13 +0100

    Mark most LFSText functions as private

--- src/text_encoding/lfs_text.gd
+++ src/text_encoding/lfs_text.gd
@@ -1,6 +1,13 @@
 class_name LFSText
 extends RefCounted
 
+## Text encoding utility class
+##
+## This class handles text encoding and color conversion from UTF8 to LFS and vice versa.
+## Most functions are for internal use only and are called automatically in [InSimPacket] functions.
+## [method unicode_to_lfs_string] can be used to get an "intermediate representation", where
+## everything is converted, but characters are encoded in UTF16 instead of proper LFS format
+## (which removes intermediate zeros).
 
 enum ColorCode {BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE}
 
@@ -42,6 +49,7 @@ const COLORS: Array[Color] = [
 const FALLBACK_CHARACTER := "?"
 
 
+## Converts BBCode color tags to LFS colors (only works for LFS preset colors, see [enum ColorCode].
 static func bbcode_to_lfs_colors(text: String) -> String:
 	var lfs_text := text
 	var regex := RegEx.create_from_string(r"(?U)(?:\^[89])?\[color=#([A-Fa-f0-9]+)\](.+)\[/color\]")
@@ -59,20 +67,124 @@ static func bbcode_to_lfs_colors(text: String) -> String:
 	return lfs_text
 
 
-static func is_multibyte(code_page: String, char: int) -> bool:
-	match code_page:
-		"^L", "^8", "^G", "^C", "^E", "^T", "^B":
-			return false
-		"^J":
-			return (char > 0x80 and char < 0xa0) or (char >= 0xe0 and char < 0xfd)
-		"^H", "^S", "^K":
-			return char > 0x80 and char < 0xff
-		_:
-			push_error("Code page unknown: %s" % [char])
-	return false
+## Converts a text string in binary LFS format to a UTF8 string.
+static func lfs_bytes_to_unicode(buffer: PackedByteArray) -> String:
+	var current_code_page := "^L"
+	var message := ""
+	var block_start := 0
+	var block_end := 0
+
+	var skip_next := false
+	for i in buffer.size():
+		if skip_next:
+			skip_next = false
+			continue
+		if buffer[i] == 0:
+			if block_start < block_end:
+				message += _get_string_from_bytes(buffer.slice(block_start, block_end), current_code_page)
+			break
+		elif _is_multibyte(current_code_page, buffer[i]):
+			block_end += 2
+			skip_next = true
+		elif buffer[i] == 0x5e:
+			# Found "^"
+			var code_page_check := "^%s" % [String.chr(buffer[i + 1])]
+			if CODE_PAGES.has(code_page_check):
+				if block_start < block_end:
+					message += _get_string_from_bytes(buffer.slice(block_start, block_end), current_code_page)
+				current_code_page = code_page_check
+				if buffer[i + 1] == 0x38:
+					block_start = i
+				else:
+					block_start = i + 2
+				block_end = i + 2
+				skip_next = true
+			else:
+				block_end += 2
+				skip_next = true
+		else:
+			block_end += 1
+	for i in SPECIAL_CHARACTERS.size():
+		var regexp := RegEx.create_from_string("\\%s" % [SPECIAL_CHARACTERS.keys()[i]])
+		message = regexp.sub(message, SPECIAL_CHARACTERS.values()[i], true)
+	message = message.replace("^^", "^")
+	return message
 
 
-static func get_bytes_from_string(code: int, code_page: String, keep_utf16: bool) -> PackedByteArray:
+## Converts LFS colors to BBCode color tags, for displaying text in [RichTextLabel] or html pages.
+static func lfs_colors_to_bbcode(text: String) -> String:
+	var colored_text := text
+	var color_regex := RegEx.create_from_string(r"\^\d")
+	var regex_match := color_regex.search(colored_text)
+	var colors_found := false
+	while regex_match:
+		var color_index := regex_match.strings[0].right(1).to_int()
+		if color_index >= 8:
+			colored_text = color_regex.sub(colored_text, "[/color]" if colors_found else "")
+			colors_found = false
+		else:
+			colored_text = color_regex.sub(colored_text,
+					"%s[color=#%s]" % ["[/color]" if colors_found else "",
+					COLORS[color_index].to_html(false)])
+			colors_found = true
+		regex_match = color_regex.search(colored_text)
+	if colors_found:
+		colored_text += "[/color]"
+	return colored_text
+
+
+## Removes all colors from [param text], including LFS colors and BBCode tags.
+static func strip_colors(text: String) -> String:
+	var result := text
+	var regex := RegEx.create_from_string(r"\^\d")
+	var regex_match := regex.search(result)
+	while regex_match:
+		var color_code := regex_match.strings[0].substr(1, 1).to_int()
+		result = regex.sub(result, "^L" if color_code == 9 else "")
+		regex_match = regex.search(result)
+	regex = RegEx.create_from_string(r"(?U)\[color=#[A-Fa-f0-9]\](.+)\[/color\]")
+	regex_match = regex.search(result)
+	while regex_match:
+		result = regex.sub(result, regex_match.strings[1])
+		regex_match = regex.search(result)
+	return result
+
+
+## Converts [param text] from UTF8 to LFS encoding as a [PackedByteArray]. If [param keep_utf16] is
+## true, the array can be converted back to text as UTF16.
+static func unicode_to_lfs_bytes(text: String, keep_utf16 := false) -> PackedByteArray:
+	var page := "L"
+	var message := _translate_specials(text)
+	var buffer := PackedByteArray()
+	for i in message.length():
+		if message.unicode_at(i) < 128:
+			buffer.append_array(message[i].to_utf16_buffer())
+			continue
+		var temp_bytes := _get_bytes_from_string(message.unicode_at(i), page, keep_utf16)
+		if not temp_bytes.is_empty():
+			buffer.append_array(temp_bytes)
+		else:
+			var code_page_found := false
+			for code_page: String in CODE_PAGES.keys():
+				temp_bytes = _get_bytes_from_string(message.unicode_at(i), code_page.substr(1, 1), keep_utf16)
+				if not temp_bytes.is_empty():
+					code_page_found = true
+					page = code_page.substr(1, 1)
+					buffer.append_array(code_page.to_utf16_buffer())
+					buffer.append_array(temp_bytes)
+					break
+			if not code_page_found:
+				buffer.append_array(_translate_specials(FALLBACK_CHARACTER).to_utf16_buffer())
+	return buffer
+
+
+## Returns an intermediate representation of [param text] in UTF16, ready to be converted to LFS
+## format by removing intermediate zeros.
+static func unicode_to_lfs_string(text: String) -> String:
+	return unicode_to_lfs_bytes(text, true).get_string_from_utf16()
+
+
+static func _get_bytes_from_string(code: int, code_page: String, keep_utf16: bool) -> PackedByteArray:
 	if not LFSCodePages.CODE_PAGE_TABLES.has(code_page):
 		return []
 	for key in LFSCodePages.CODE_PAGE_TABLES.keys():
@@ -91,7 +203,7 @@ static func get_bytes_from_string(code: int, code_page: String, keep_utf16: bool
 	return []
 
 
-static func get_string_from_bytes(buffer: PackedByteArray, code_page: String) -> String:
+static func _get_string_from_bytes(buffer: PackedByteArray, code_page: String) -> String:
 	var text := ""
 	var page := code_page
 	if page.length() > 1:
@@ -123,118 +235,21 @@ static func get_string_from_bytes(buffer: PackedByteArray, code_page: String) ->
 	return text
 
 
-static func lfs_bytes_to_unicode(buffer: PackedByteArray) -> String:
-	var current_code_page := "^L"
-	var message := ""
-	var block_start := 0
-	var block_end := 0
-
-	var skip_next := false
-	for i in buffer.size():
-		if skip_next:
-			skip_next = false
-			continue
-		if buffer[i] == 0:
-			if block_start < block_end:
-				message += get_string_from_bytes(buffer.slice(block_start, block_end), current_code_page)
-			break
-		elif is_multibyte(current_code_page, buffer[i]):
-			block_end += 2
-			skip_next = true
-		elif buffer[i] == 0x5e:
-			# Found "^"
-			var code_page_check := "^%s" % [String.chr(buffer[i + 1])]
-			if CODE_PAGES.has(code_page_check):
-				if block_start < block_end:
-					message += get_string_from_bytes(buffer.slice(block_start, block_end), current_code_page)
-				current_code_page = code_page_check
-				if buffer[i + 1] == 0x38:
-					block_start = i
-				else:
-					block_start = i + 2
-				block_end = i + 2
-				skip_next = true
-			else:
-				block_end += 2
-				skip_next = true
-		else:
-			block_end += 1
-	for i in SPECIAL_CHARACTERS.size():
-		var regexp := RegEx.create_from_string("\\%s" % [SPECIAL_CHARACTERS.keys()[i]])
-		message = regexp.sub(message, SPECIAL_CHARACTERS.values()[i], true)
-	message = message.replace("^^", "^")
-	return message
+static func _is_multibyte(code_page: String, char: int) -> bool:
+	match code_page:
+		"^L", "^8", "^G", "^C", "^E", "^T", "^B":
+			return false
+		"^J":
+			return (char > 0x80 and char < 0xa0) or (char >= 0xe0 and char < 0xfd)
+		"^H", "^S", "^K":
+			return char > 0x80 and char < 0xff
+		_:
+			push_error("Code page unknown: %s" % [char])
+	return false
 
 
-static func lfs_colors_to_bbcode(text: String) -> String:
-	var colored_text := text
-	var color_regex := RegEx.create_from_string(r"\^\d")
-	var regex_match := color_regex.search(colored_text)
-	var colors_found := false
-	while regex_match:
-		var color_index := regex_match.strings[0].right(1).to_int()
-		if color_index >= 8:
-			colored_text = color_regex.sub(colored_text, "[/color]" if colors_found else "")
-			colors_found = false
-		else:
-			colored_text = color_regex.sub(colored_text,
-					"%s[color=#%s]" % ["[/color]" if colors_found else "",
-					COLORS[color_index].to_html(false)])
-			colors_found = true
-		regex_match = color_regex.search(colored_text)
-	if colors_found:
-		colored_text += "[/color]"
-	return colored_text
-
-
-static func strip_colors(text: String) -> String:
-	var result := text
-	var regex := RegEx.create_from_string(r"\^\d")
-	var regex_match := regex.search(result)
-	while regex_match:
-		var color_code := regex_match.strings[0].substr(1, 1).to_int()
-		result = regex.sub(result, "^L" if color_code == 9 else "")
-		regex_match = regex.search(result)
-	regex = RegEx.create_from_string(r"(?U)\[color=#[A-Fa-f0-9]\](.+)\[/color\]")
-	regex_match = regex.search(result)
-	while regex_match:
-		result = regex.sub(result, regex_match.strings[1])
-		regex_match = regex.search(result)
-	return result
-
-
-static func translate_specials(text: String) -> String:
+static func _translate_specials(text: String) -> String:
 	var message := text
 	for i in SPECIAL_CHARACTERS.size():
 		message = message.replace(SPECIAL_CHARACTERS.values()[i], SPECIAL_CHARACTERS.keys()[i])
 	return message
-
-
-static func unicode_to_lfs_bytes(text: String, keep_utf16 := false) -> PackedByteArray:
-	var page := "L"
-	var message := translate_specials(text)
-	var buffer := PackedByteArray()
-	for i in message.length():
-		if message.unicode_at(i) < 128:
-			buffer.append_array(message[i].to_utf16_buffer())
-			continue
-		var temp_bytes := get_bytes_from_string(message.unicode_at(i), page, keep_utf16)
-		if not temp_bytes.is_empty():
-			buffer.append_array(temp_bytes)
-		else:
-			var code_page_found := false
-			for code_page: String in CODE_PAGES.keys():
-				temp_bytes = get_bytes_from_string(message.unicode_at(i), code_page.substr(1, 1), keep_utf16)
-				if not temp_bytes.is_empty():
-					code_page_found = true
-					page = code_page.substr(1, 1)
-					buffer.append_array(code_page.to_utf16_buffer())
-					buffer.append_array(temp_bytes)
-					break
-			if not code_page_found:
-				buffer.append_array(translate_specials(FALLBACK_CHARACTER).to_utf16_buffer())
-	return buffer
-
-
-static func _unicode_to_lfs_string(text: String) -> String:
-	return unicode_to_lfs_bytes(text, true).get_string_from_utf16()
