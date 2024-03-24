8206bd1f7a890c9e8120e0f1fc08f8551171eed4
Cyril K.B. <53737317+Cykyrios@users.noreply.github.com>
2024-03-24 23:30:21 +0100

    Fix read/write mixup in OCO packet

--- src/insim/packet/insim_oco_packet.gd
+++ src/insim/packet/insim_oco_packet.gd
@@ -19,17 +19,13 @@ func _init() -> void:
 	sendable = true
 
 
-func _decode_packet(packet: PackedByteArray) -> void:
-	var packet_size := packet.size()
-	if packet_size != PACKET_SIZE:
-		push_error("%s packet expected size %d, got %d." % [InSim.Packet.keys()[type], size, packet_size])
-		return
-	super(packet)
-	zero = read_byte()
-	oco_action = read_byte() as InSim.OCOAction
-	index = read_byte()
-	identifier = read_byte()
-	data = read_byte()
+func _fill_buffer() -> void:
+	super()
+	add_byte(zero)
+	add_byte(oco_action)
+	add_byte(index)
+	add_byte(identifier)
+	add_byte(data)
 
 
 func _get_data_dictionary() -> Dictionary:
