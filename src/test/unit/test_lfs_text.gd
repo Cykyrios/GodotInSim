6bc3c55d1ba44364cd0fedfa7cc0b6008831e6a0
Cykyrios <cykyrios@gmail.com>
2024-07-26 15:12:18 +0200

    Move unit tests to GUT v9.3.0
    Obviously requires GUT in any project that needs to run the tests.

---
+++ src/test/unit/test_lfs_text.gd
@@ -0,0 +1,19 @@
+extends GutTest
+
+
+func test_lfs_to_unicode() -> void:
+	assert_eq(LFSText.lfs_string_to_unicode("Test string, ASCII only."), "Test string, ASCII only.")
+	assert_eq(LFSText.lfs_string_to_unicode("Other scripts and special characters^c ^J鏺陻質, ^^a^a^v^d."),
+			"Other scripts and special characters: 日本語, ^a*|\\.")
+	assert_eq(LFSText.lfs_string_to_unicode("^Eì ^Cø ^JÏ"), "ě ш ﾏ")
+	assert_eq(LFSText.lfs_string_to_unicode("^72^45 ^7B2^4^JÏ ^1Ayoub"), "^72^45 ^7B2^4ﾏ ^1Ayoub")
+	assert_eq(LFSText.lfs_string_to_unicode("^405 ^J¢^7Ï§^4£ ^7TJ"), "^405 ｢^7ﾏｧ^4｣ ^7TJ")
+
+
+func test_unicode_to_lfs() -> void:
+	assert_eq(LFSText.unicode_to_lfs_string("Test string, ASCII only."), "Test string, ASCII only.")
+	assert_eq(LFSText.unicode_to_lfs_string("Other scripts and special characters: 日本語, ^a*|\\."),
+			"Other scripts and special characters^c ^J鏺陻質, ^^a^a^v^d.")
+	assert_eq(LFSText.unicode_to_lfs_string("ě ш ﾏ"), "^Eì ^Cø ^JÏ")
+	assert_eq(LFSText.unicode_to_lfs_string("^72^45 ^7B2^4ﾏ ^1Ayoub"), "^72^45 ^7B2^4^JÏ ^1Ayoub")
+	assert_eq(LFSText.unicode_to_lfs_string("^405 ｢^7ﾏｧ^4｣ ^7TJ"), "^405 ^J¢^7Ï§^4£ ^7TJ")
---
+++ src/test/unit/test_time_string.gd
@@ -0,0 +1,20 @@
+extends GutTest
+
+
+func test_time_float_to_string() -> void:
+	assert_eq(GISUtils.get_time_string_from_seconds(0), "00:00.000")
+	assert_eq(GISUtils.get_time_string_from_seconds(42.21), "42.210")
+	assert_eq(GISUtils.get_time_string_from_seconds(-10), "00:00.000")
+	assert_eq(GISUtils.get_time_string_from_seconds(3600), "1:00:00.000")
+	assert_eq(GISUtils.get_time_string_from_seconds(3599.99), "59:59.990")
+
+
+func test_time_string_to_float() -> void:
+	assert_eq(GISUtils.get_seconds_from_time_strings("00:00.00"), 0.0)
+	assert_eq(GISUtils.get_seconds_from_time_strings("12:34.56"), 754.56)
+	assert_eq(GISUtils.get_seconds_from_time_strings("12:0:34.56"), 43234.56)
+	assert_eq(GISUtils.get_seconds_from_time_strings("99:59:59.99"), 359_999.99)
+	assert_eq(GISUtils.get_seconds_from_time_strings("100:0:0.0"), 360_000.0)
+	assert_eq(GISUtils.get_seconds_from_time_strings("0:70:80.9990"), 4280.999)
+	assert_eq(GISUtils.get_seconds_from_time_strings("8."), 8.0)
+	assert_eq(GISUtils.get_seconds_from_time_strings("0:1:0."), 60.0)
