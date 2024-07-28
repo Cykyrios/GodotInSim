3fc6327cb5c5effa5c6a512222ce0eb5cbfb97c3
Cykyrios <cykyrios@gmail.com>
2024-07-28 11:27:35 +0200

    Fix GISUtils time string function name in unit tests

--- src/test/unit/test_time_string.gd
+++ src/test/unit/test_time_string.gd
@@ -10,11 +10,11 @@ func test_time_float_to_string() -> void:
 
 
 func test_time_string_to_float() -> void:
-	assert_eq(GISUtils.get_seconds_from_time_strings("00:00.00"), 0.0)
-	assert_eq(GISUtils.get_seconds_from_time_strings("12:34.56"), 754.56)
-	assert_eq(GISUtils.get_seconds_from_time_strings("12:0:34.56"), 43234.56)
-	assert_eq(GISUtils.get_seconds_from_time_strings("99:59:59.99"), 359_999.99)
-	assert_eq(GISUtils.get_seconds_from_time_strings("100:0:0.0"), 360_000.0)
-	assert_eq(GISUtils.get_seconds_from_time_strings("0:70:80.9990"), 4280.999)
-	assert_eq(GISUtils.get_seconds_from_time_strings("8."), 8.0)
-	assert_eq(GISUtils.get_seconds_from_time_strings("0:1:0."), 60.0)
+	assert_eq(GISUtils.get_seconds_from_time_string("00:00.00"), 0.0)
+	assert_eq(GISUtils.get_seconds_from_time_string("12:34.56"), 754.56)
+	assert_eq(GISUtils.get_seconds_from_time_string("12:0:34.56"), 43234.56)
+	assert_eq(GISUtils.get_seconds_from_time_string("99:59:59.99"), 359_999.99)
+	assert_eq(GISUtils.get_seconds_from_time_string("100:0:0.0"), 360_000.0)
+	assert_eq(GISUtils.get_seconds_from_time_string("0:70:80.9990"), 4280.999)
+	assert_eq(GISUtils.get_seconds_from_time_string("8."), 8.0)
+	assert_eq(GISUtils.get_seconds_from_time_string("0:1:0."), 60.0)
