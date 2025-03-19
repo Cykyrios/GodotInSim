207d33259dba07369fc39c989087c344d4e3f784
Cyril B. <53737317+Cykyrios@users.noreply.github.com>
2025-03-19 06:57:52 +0100

    Fix unit conversion tests warnings

--- src/test/unit/test_unit_conversion.gd
+++ src/test/unit/test_unit_conversion.gd
@@ -5,11 +5,16 @@ var epsilon := 1e-5
 
 
 var acceleration_params := [
-	[10.0, GISUtils.AccelerationUnit.METER_PER_SECOND_SQUARED, GISUtils.AccelerationUnit.G, 1.019_367_992],
+	[10.0, GISUtils.AccelerationUnit.METER_PER_SECOND_SQUARED,
+			GISUtils.AccelerationUnit.G, 1.019_367_992],
 	[1.0, GISUtils.AccelerationUnit.G, GISUtils.AccelerationUnit.METER_PER_SECOND_SQUARED, 9.81],
 	]
 func test_acceleration_conversion(params: Array = use_parameters(acceleration_params)) -> void:
-	assert_almost_eq(GISUtils.convert_acceleration(params[0], params[1], params[2]), params[3], epsilon)
+	assert_almost_eq(GISUtils.convert_acceleration(
+		params[0] as float,
+		params[1] as GISUtils.AccelerationUnit,
+		params[2] as GISUtils.AccelerationUnit
+	), params[3] as float, epsilon)
 
 
 var angle_params := [
@@ -17,14 +22,22 @@ var angle_params := [
 	[10.0, GISUtils.AngleUnit.RADIAN, GISUtils.AngleUnit.DEGREE, 572.957_795_1],
 	]
 func test_angle_conversion(params: Array = use_parameters(angle_params)) -> void:
-	assert_almost_eq(GISUtils.convert_angle(params[0], params[1], params[2]), params[3], epsilon)
+	assert_almost_eq(GISUtils.convert_angle(
+		params[0] as float,
+		params[1] as GISUtils.AngleUnit,
+		params[2] as GISUtils.AngleUnit
+	), params[3] as float, epsilon)
 
 
 var force_params := [
 	[10.0, GISUtils.ForceUnit.NEWTON, GISUtils.ForceUnit.NEWTON, 10.0],
 	]
 func test_force_conversion(params: Array = use_parameters(force_params)) -> void:
-	assert_almost_eq(GISUtils.convert_force(params[0], params[1], params[2]), params[3], epsilon)
+	assert_almost_eq(GISUtils.convert_force(
+		params[0] as float,
+		params[1] as GISUtils.ForceUnit,
+		params[2] as GISUtils.ForceUnit
+	), params[3] as float, epsilon)
 
 
 var length_params := [
@@ -32,7 +45,11 @@ var length_params := [
 	[10.0, GISUtils.LengthUnit.KILOMETER, GISUtils.LengthUnit.MILE, 6.213_711_922],
 	]
 func test_length_conversion(params: Array = use_parameters(length_params)) -> void:
-	assert_almost_eq(GISUtils.convert_length(params[0], params[1], params[2]), params[3], epsilon)
+	assert_almost_eq(GISUtils.convert_length(
+		params[0] as float,
+		params[1] as GISUtils.LengthUnit,
+		params[2] as GISUtils.LengthUnit
+	), params[3] as float, epsilon)
 
 
 var mass_params := [
@@ -40,14 +57,22 @@ var mass_params := [
 	[10.0, GISUtils.MassUnit.POUND, GISUtils.MassUnit.TONNE, 0.004_535_923_7],
 	]
 func test_mass_conversion(params: Array = use_parameters(mass_params)) -> void:
-	assert_almost_eq(GISUtils.convert_mass(params[0], params[1], params[2]), params[3], epsilon)
+	assert_almost_eq(GISUtils.convert_mass(
+		params[0] as float,
+		params[1] as GISUtils.MassUnit,
+		params[2] as GISUtils.MassUnit
+	), params[3] as float, epsilon)
 
 
 var power_params := [
 	[10.0, GISUtils.PowerUnit.WATT, GISUtils.PowerUnit.WATT, 10.0],
 	]
 func test_power_conversion(params: Array = use_parameters(power_params)) -> void:
-	assert_almost_eq(GISUtils.convert_power(params[0], params[1], params[2]), params[3], epsilon)
+	assert_almost_eq(GISUtils.convert_power(
+		params[0] as float,
+		params[1] as GISUtils.PowerUnit,
+		params[2] as GISUtils.PowerUnit
+	), params[3] as float, epsilon)
 
 
 var speed_params := [
@@ -56,7 +81,11 @@ var speed_params := [
 	[100.0, GISUtils.SpeedUnit.MPH, GISUtils.SpeedUnit.KPH, 160.9344],
 	]
 func test_speed_conversion(params: Array = use_parameters(speed_params)) -> void:
-	assert_almost_eq(GISUtils.convert_speed(params[0], params[1], params[2]), params[3], epsilon)
+	assert_almost_eq(GISUtils.convert_speed(
+		params[0] as float,
+		params[1] as GISUtils.SpeedUnit,
+		params[2] as GISUtils.SpeedUnit
+	), params[3] as float, epsilon)
 
 
 var time_params := [
@@ -65,4 +94,8 @@ var time_params := [
 	[2.5, GISUtils.TimeUnit.HOUR, GISUtils.TimeUnit.MINUTE, 150.0],
 	]
 func test_time_conversion(params: Array = use_parameters(time_params)) -> void:
-	assert_almost_eq(GISUtils.convert_time(params[0], params[1], params[2]), params[3], epsilon)
+	assert_almost_eq(GISUtils.convert_time(
+		params[0] as float,
+		params[1] as GISUtils.TimeUnit,
+		params[2] as GISUtils.TimeUnit
+	), params[3] as float, epsilon)
