b95dc39d2e0fcf0cbd68d032452fd86898f79eec
Cyril B. <53737317+Cykyrios@users.noreply.github.com>
2025-03-22 23:55:26 +0100

    Add get_buffer() and gis_position to OutSimMain

--- src/outsim/struct/outsim_main.gd
+++ src/outsim/struct/outsim_main.gd
@@ -3,6 +3,7 @@ extends RefCounted
 
 
 const STRUCT_SIZE := 60
+const POSITION_MULTIPLIER := 65536.0
 
 var ang_vel := Vector3.ZERO
 var heading := 0.0
@@ -12,10 +13,33 @@ var accel := Vector3.ZERO
 var vel := Vector3.ZERO
 var pos := Vector3i.ZERO
 
+var gis_position := Vector3.ZERO
+
 
 func _to_string() -> String:
 	return "AngVel:%v, Heading:%f, Pitch:%f, Roll:%f, Accel:%v, Vel:%v, Pos:%v" % \
 			[ang_vel, heading, pitch, roll, accel, vel, pos]
+
+
+func get_buffer() -> PackedByteArray:
+	var buffer := PackedByteArray()
+	var _discard := buffer.resize(STRUCT_SIZE)
+	buffer.encode_float(0, ang_vel.x)
+	buffer.encode_float(4, ang_vel.y)
+	buffer.encode_float(8, ang_vel.z)
+	buffer.encode_float(12, heading)
+	buffer.encode_float(16, pitch)
+	buffer.encode_float(20, roll)
+	buffer.encode_float(24, accel.x)
+	buffer.encode_float(28, accel.y)
+	buffer.encode_float(32, accel.z)
+	buffer.encode_float(36, vel.x)
+	buffer.encode_float(40, vel.y)
+	buffer.encode_float(44, vel.z)
+	buffer.encode_u32(48, pos.x)
+	buffer.encode_u32(52, pos.y)
+	buffer.encode_u32(56, pos.z)
+	return buffer
 
 
 func set_from_buffer(buffer: PackedByteArray) -> void:
@@ -30,3 +54,4 @@ func set_from_buffer(buffer: PackedByteArray) -> void:
 	accel = Vector3(buffer.decode_float(24), buffer.decode_float(28), buffer.decode_float(32))
 	vel = Vector3(buffer.decode_float(36), buffer.decode_float(40), buffer.decode_float(44))
 	pos = Vector3i(buffer.decode_u32(48), buffer.decode_u32(52), buffer.decode_u32(56))
+	gis_position = pos / POSITION_MULTIPLIER
