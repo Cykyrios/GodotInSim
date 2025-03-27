1e53941b95a8e58a121815ee0f45397f193cd17d
Cyril B. <53737317+Cykyrios@users.noreply.github.com>
2025-03-27 23:57:32 +0100

    Add lyt parser

---
+++ src/lyt/lyt_file.gd
@@ -0,0 +1,54 @@
+class_name LYTFile
+extends LFSPacket
+
+
+const HEADER_SIZE := 12
+
+var header := ""
+var version := 0
+var revision := 0
+var num_objects := 0
+var laps := 0
+var flags := 0
+var objects: Array[LYTObject] = []
+
+
+func read_from_path(path: String) -> void:
+	var file := FileAccess.open(path, FileAccess.READ)
+	var error := FileAccess.get_open_error()
+	if error != OK:
+		push_error("Error code %d occurred while opening file at %s" % [error, path])
+		return
+	buffer = file.get_buffer(file.get_length())
+	header = read_string(6, false)
+	version = read_byte()
+	revision = read_byte()
+	num_objects = read_word()
+	laps = read_byte()
+	flags = read_byte()
+	if buffer.size() != HEADER_SIZE + num_objects * LYTObject.STRUCT_SIZE:
+		push_error("LYT file data does not match object count (expected %d, found %d)" % [
+				num_objects, int((buffer.size() - HEADER_SIZE) as float / LYTObject.STRUCT_SIZE)])
+		return
+	for i in num_objects:
+		objects.append(LYTObject.create_from_buffer(read_buffer(LYTObject.STRUCT_SIZE)))
+
+
+func write_to_file(path: String) -> void:
+	var file := FileAccess.open(path, FileAccess.WRITE)
+	var error := FileAccess.get_open_error()
+	if error != OK:
+		push_error("Failed to open file for writing: error %d" % [error])
+		return
+	resize_buffer(HEADER_SIZE + num_objects * LYTObject.STRUCT_SIZE)
+	data_offset = 0
+	var _discard := add_string(6, "LFSLYT", false)
+	add_byte(version)
+	add_byte(revision)
+	add_word(num_objects)
+	add_byte(laps)
+	add_byte(flags)
+	for i in num_objects:
+		add_buffer(objects[i].get_buffer())
+	if not file.store_buffer(buffer):
+		push_error("Failed to write layout to file")
---
+++ src/lyt/lyt_object.gd
@@ -0,0 +1,80 @@
+class_name LYTObject
+extends RefCounted
+
+
+const STRUCT_SIZE := 8
+
+const XY_MULTIPLIER := 16.0
+const Z_MULTIPLIER := 4.0
+const HEADING_MULTIPLIER := 256 / 360.0
+
+var x := 0
+var y := 0
+var z := 0
+var flags := 0
+var index := 0
+var heading := 0
+
+var gis_position := Vector3.ZERO
+var gis_heading := 0.0
+
+
+static func create(
+	obj_x: int, obj_y: int, obj_z: int, obj_heading: int, obj_flags: int, obj_index: int
+) -> LYTObject:
+	var object := LYTObject.new()
+	object.x = obj_x
+	object.y = obj_y
+	object.z = obj_z
+	object.gis_position = Vector3(
+		obj_x / XY_MULTIPLIER,
+		obj_y / XY_MULTIPLIER,
+		obj_z / Z_MULTIPLIER
+	)
+	object.flags = obj_flags
+	object.index = obj_index
+	object.heading = obj_heading
+	object.gis_heading = deg_to_rad(object.heading / HEADING_MULTIPLIER - 180)
+	return object
+
+
+static func create_from_gis(
+	obj_position: Vector3, obj_heading: float, obj_flags: int, obj_index: int
+) -> LYTObject:
+	var object := LYTObject.new()
+	object.gis_position = obj_position
+	object.x = roundi(obj_position.x * XY_MULTIPLIER)
+	object.y = roundi(obj_position.y * XY_MULTIPLIER)
+	object.z = roundi(obj_position.z * Z_MULTIPLIER)
+	object.flags = obj_flags
+	object.index = obj_index
+	object.heading = roundi((rad_to_deg(obj_heading) + 180) * HEADING_MULTIPLIER)
+	return object
+
+
+## Returns a [LYTObject] from the provided [param buffer]. Returns a blank object if [param buffer]
+## is the wrong size.
+static func create_from_buffer(buffer: PackedByteArray) -> LYTObject:
+	if buffer.size() != STRUCT_SIZE:
+		push_warning("Buffer size does not match LYTObject struct size.")
+		return LYTObject.new()
+	var object := LYTObject.new()
+	object.x = buffer.decode_s16(0)
+	object.y = buffer.decode_s16(2)
+	object.z = buffer.decode_u8(4)
+	object.flags = buffer.decode_u8(5)
+	object.index = buffer.decode_u8(6)
+	object.heading = buffer.decode_u8(7)
+	return object
+
+
+func get_buffer() -> PackedByteArray:
+	var buffer := PackedByteArray()
+	var _resize := buffer.resize(STRUCT_SIZE)
+	buffer.encode_s16(0, x)
+	buffer.encode_s16(2, y)
+	buffer.encode_u8(4, z)
+	buffer.encode_u8(5, flags)
+	buffer.encode_u8(6, index)
+	buffer.encode_u8(7, heading)
+	return buffer
