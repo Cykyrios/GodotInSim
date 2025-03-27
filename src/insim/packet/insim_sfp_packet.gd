68c8c9ba6f1b4cc014f18ebcb25c36fb9ef33166
Cyril B. <53737317+Cykyrios@users.noreply.github.com>
2025-03-27 10:34:19 +0100

    Fix InSimSFPPacket use of InSim.State enum

--- src/insim/packet/insim_sfp_packet.gd
+++ src/insim/packet/insim_sfp_packet.gd
@@ -14,7 +14,7 @@ const PACKET_SIZE := 8
 const PACKET_TYPE := InSim.Packet.ISP_SFP
 var zero := 0
 
-var flag := InSim.State  ## the state to set
+var flag := InSim.State.ISS_SHIFTU_NO_OPT  ## the state to set
 var off_on := 0  ## 0 = off / 1 = on
 var sp3 := 0
 
@@ -28,7 +28,7 @@ func _init() -> void:
 func _fill_buffer() -> void:
 	super()
 	add_byte(zero)
-	add_word(InSim.State.values()[flag] as int)
+	add_word(flag)
 	add_byte(off_on)
 	add_byte(sp3)
 
