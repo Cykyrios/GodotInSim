74014f8d75dd78f1a0f82588fd2bde1086d9a3e5
Cyril B. <53737317+Cykyrios@users.noreply.github.com>
2025-04-03 08:09:44 +0200

    Fix run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -5,4 +5,4 @@
 # godot is assumed to be in PATH, and GdUnit4 located at addons/gdUnit4
 export GODOT_BIN=godot
 
-./addons/gdUnit4/runtest.sh --headless --ignoreHeadlessMode -c -a ${godot_insim}/test
+./addons/gdUnit4/runtest.sh --headless --ignoreHeadlessMode -c -a addons/godot_insim/test
