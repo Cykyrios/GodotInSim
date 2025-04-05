0dcf795f21b29f2c166f72522630d41987e0bdee
Cyril B. <53737317+Cykyrios@users.noreply.github.com>
2025-04-05 14:38:57 +0200

    Add create function to InSimInitializationData

--- addons/godot_insim/src/insim/insim_initialization_data.gd
+++ addons/godot_insim/src/insim/insim_initialization_data.gd
@@ -18,3 +18,17 @@ var interval := 0:
 
 var admin := ""
 var i_name := "Godot InSim"
+
+
+static func create(
+	init_name: String, init_admin: String, init_flags: int, init_prefix: String,
+	init_interval := 0, init_udp := 0
+) -> InSimInitializationData:
+	var init_data := InSimInitializationData.new()
+	init_data.i_name = init_name
+	init_data.admin = init_admin
+	init_data.flags = init_flags
+	init_data.prefix = init_prefix
+	init_data.interval = init_interval
+	init_data.udp_port = init_udp
+	return init_data
