e20dba62cec93c8f2ca197f08d7d581465c0c98a
Cyril B. <53737317+Cykyrios@users.noreply.github.com>
2025-06-03 13:47:39 +0200

    Update XML converter, add undocumented enum option

--- docs_generator/generate_docs.sh
+++ docs_generator/generate_docs.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-bash addons/gdscript_xml_converter/bin/convert_xml.sh -f mdx -i docs -o output -x res://addons/godot_insim/src
+bash addons/gdscript_xml_converter/bin/convert_xml.sh \
+	-f mdx -i docs -o output -x res://addons/godot_insim/src \
+	--allow-undocumented-enums
 code=$?
 if [ $code -ne 0 ]; then
 	exit $code
 fi
-mkdir -p ./website/docs/class_ref
 mv ./output/* ./website/docs/class_ref/
