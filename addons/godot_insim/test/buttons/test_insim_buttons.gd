1487cd27199f9dc38f1a0ff73347c0e0ffa0c8b3
Cyril B. <53737317+Cykyrios@users.noreply.github.com>
2025-06-10 08:26:00 +0200

    Add tests for button deletion functions

--- addons/godot_insim/test/buttons/test_insim_buttons.gd
+++ addons/godot_insim/test/buttons/test_insim_buttons.gd
@@ -45,6 +45,44 @@ func test_compact_bfn_packets() -> void:
 	var _test := assert_array(buffer_compacted).is_equal(buffer_expected)
 
 
+func test_delete_buttons_by_id() -> void:
+	insim.add_button([1, 2], Vector2i.ZERO, Vector2i.ZERO, 0, "", "button")
+	insim.add_button([2, 3], Vector2i.ZERO, Vector2i.ZERO, 0, "", "test_button")
+	insim.add_button([1, 2], Vector2i.ZERO, Vector2i.ZERO, 0, "", "hello")
+	var packets := insim.buttons.delete_buttons_by_id([1, 2], 1, 2)
+	var _test: GdUnitAssert = assert_int(packets.size()).is_equal(2)
+	# NOTE: UCID 1 only has buttons 0 and 1, but only the click_id is checked for existence, so
+	# the expected result is still click_id == 1 and max_id == 2.
+	_test = assert_array([
+		packets[0].ucid, packets[0].click_id, packets[0].click_max,
+		packets[1].ucid, packets[1].click_id, packets[1].click_max,
+	]).is_equal([1, 1, 2, 2, 1, 2])
+
+
+func test_delete_buttons_by_name() -> void:
+	insim.add_button([1, 2], Vector2i.ZERO, Vector2i.ZERO, 0, "", "button")
+	insim.add_button([2, 3], Vector2i.ZERO, Vector2i.ZERO, 0, "", "test_button")
+	insim.add_button([1, 2], Vector2i.ZERO, Vector2i.ZERO, 0, "", "hello")
+	var packets := insim.buttons.delete_buttons_by_prefix([1, 2], "button")
+	var _test: GdUnitAssert = assert_int(packets.size()).is_equal(2)
+	_test = assert_array([
+		packets[0].ucid, packets[0].click_id, packets[0].click_max,
+		packets[1].ucid, packets[1].click_id, packets[1].click_max,
+	]).is_equal([1, 0, 0, 2, 0, 0])
+
+
+func test_delete_buttons_by_prefix() -> void:
+	insim.add_button([1, 2], Vector2i.ZERO, Vector2i.ZERO, 0, "", "a/b")
+	insim.add_button([2, 3], Vector2i.ZERO, Vector2i.ZERO, 0, "", "c/d/e")
+	insim.add_button([1, 2], Vector2i.ZERO, Vector2i.ZERO, 0, "", "c/desktop")
+	var packets := insim.buttons.delete_buttons_by_prefix([1, 2], "c/d")
+	var _test: GdUnitAssert = assert_int(packets.size()).is_equal(2)
+	_test = assert_array([
+		packets[0].ucid, packets[0].click_id, packets[0].click_max,
+		packets[1].ucid, packets[1].click_id, packets[1].click_max,
+	]).is_equal([1, 1, 0, 2, 1, 2])
+
+
 func test_delete_buttons_by_regex() -> void:
 	insim.add_button([1, 2], Vector2i.ZERO, Vector2i.ZERO, 0, "", "a/b")
 	insim.add_button([2, 3], Vector2i.ZERO, Vector2i.ZERO, 0, "", "c/d/e")
