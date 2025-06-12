bdde2e2165e92e9601eed9e0b63b3975e3454af4
Cyril B. <53737317+Cykyrios@users.noreply.github.com>
2025-06-12 09:39:35 +0200

    Allow creating LYTObjectStart from GIS values, use natural position number

--- addons/godot_insim/src/lyt/lyt_object_start.gd
+++ addons/godot_insim/src/lyt/lyt_object_start.gd
@@ -4,7 +4,10 @@ extends LYTObject
 ##
 ## Specific layout object representing a starting position (grid box).
 
-var number := 0  ## start position index (0 for 1st, 1 for 2nd, etc.)
+const MIN_POSITION := 0  ## Minimum position index ([code]start_position - 1[/code])
+const MAX_POSITION := 47  ## Maximum position index ([code]start_position - 1[/code])
+
+var start_position := 0  ## Start position (0 is not a valid number)
 
 
 ## Creates and returns a starting position object from the given parameters.
@@ -13,10 +16,23 @@ static func create(
 ) -> LYTObjectStart:
 	var object := super(obj_x, obj_y, obj_z, obj_heading, obj_flags, obj_index)
 	var start_object := LYTObjectStart.new()
-	start_object.set_from_buffer(object.get_buffer())
-	start_object.number = start_object.flags & 0x3f
+	start_object._set_from_buffer(object.get_buffer())
 	return start_object
 
 
+static func create_from_gis(
+	obj_position: Vector3, obj_heading: float, obj_flags: int, obj_index: int
+) -> LYTObjectStart:
+	var object := super(obj_position, obj_heading, obj_flags, obj_index)
+	var start_object := LYTObjectStart.new()
+	start_object._set_from_buffer(object.get_buffer())
+	return start_object
+
+
+func _set_from_buffer(buffer: PackedByteArray) -> void:
+	set_from_buffer(buffer)
+	start_position = flags & 0x3f + 1
+
+
 func _update_flags() -> void:
-	flags = (flags & ~0x3f) | number
+	flags = (flags & ~0x3f) | clampi(start_position - 1, MIN_POSITION, MAX_POSITION)
