a0db2ba585da6903e22b34df60665abb8fea2f7d
Cyril B. <53737317+Cykyrios@users.noreply.github.com>
2025-07-10 09:33:14 +0200

    Fix BTN packet color bleeding in get_pretty_text()

--- addons/godot_insim/src/insim/packet/insim_btn_packet.gd
+++ addons/godot_insim/src/insim/packet/insim_btn_packet.gd
@@ -100,7 +100,7 @@ func _get_pretty_text() -> String:
 			button_flags.append(InSim.ButtonStyle.keys()[i])
 	var target := "local" if ucid == 0 else "everyone" if ucid == 255 else "UCID %d" % [ucid]
 	return "Button for %s: ID %d, %s, %d-%d:%dx%d, \"%s\"" % [target, click_id, button_flags,
-			left, top, width, height, text]
+			left, top, width, height, text + "^9" + (" (%s^9)" % [caption] if caption else "")]
 
 
 func _set_data_from_dictionary(dict: Dictionary) -> void:
