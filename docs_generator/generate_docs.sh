6ae98ce4e6ec2851f669034e2555eaa276fba90c
Cyril B. <53737317+Cykyrios@users.noreply.github.com>
2025-08-07 13:14:55 +0200

    Allow doc generation script to set godot executable

--- docs_generator/generate_docs.sh
+++ docs_generator/generate_docs.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+while true; do
+	case "$1" in
+		--godot ) export GODOT_BIN="$2"; shift 2 ;;
+		* ) break ;;
+	esac
+done
+
 bash addons/gdscript_xml_converter/bin/convert_xml.sh \
 	-f mdx -i docs -o output -x res://addons/godot_insim/src \
 	--allow-undocumented-enums
