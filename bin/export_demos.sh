721a63297517be025e11def87496cecd00c8818c
Cyril B. <53737317+Cykyrios@users.noreply.github.com>
2025-08-07 23:57:33 +0200

    Refactor demo export logic

---
+++ bin/export_demos.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+while true; do
+	case "$1" in
+		--godot ) export GODOT_BIN="$2"; shift 2 ;;
+		* ) break ;;
+	esac
+done
+
+if [ -z "$GODOT_BIN" ]; then
+	export GODOT_BIN=godot
+fi
+
+platforms=(
+	"linux"
+	"windows"
+)
+for platform in "${platforms[@]}"; do
+	if [ "$platform" == "linux" ]; then
+		extension=".x86_64"
+	else
+		extension=".exe"
+	fi
+	directory=build/demos/$platform/bin
+	mkdir -p "$directory"
+	$GODOT_BIN --headless --export-release "${platform^} Demo Executable" "$directory"/Godot_InSim"$extension"
+	rm -f "$directory"/Godot_InSim.pck
+done
+
+demos=(
+	"InSimRelay"
+	"Layout Viewer"
+	"Packet Logger"
+	"PTH Viewer"
+	"Telemetry"
+	"Teleporter"
+)
+for demo in "${demos[@]}"; do
+	demo_file=GIS_"${demo//\ /_}"
+	$GODOT_BIN --headless --export-pack "Demo $demo" build/demos/"${demo_file}".pck
+	echo -e "#!/bin/bash\n./bin/Godot_InSim.x86_64 --main-pack ./bin/${demo_file}.pck" >> build/demos/linux/"$demo_file".sh
+	echo ".\\bin\\Godot_InSim.exe --main-pack .\\bin\\${demo_file}.pck" >> build/demos/windows/"$demo_file".bat
+done
+
+cd build/demos
+for platform in "${platforms[@]}"; do
+	if [ "$platform" == "linux" ]; then
+		extension=sh
+	else
+		extension=bat
+	fi
+	cd "$platform"
+	for script in *.$extension; do
+		chmod +x "$script"
+	done
+	cd ..
+	cp *.pck ./"$platform"/bin/
+done
+rm -f *.pck
